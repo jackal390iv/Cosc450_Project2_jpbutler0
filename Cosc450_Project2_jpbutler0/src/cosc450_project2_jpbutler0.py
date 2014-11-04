0b8cf8cb234ccb25a324de810ed1262e45e74640
walter <walter@walter.local>
2014-11-03 19:55:25 -0500

    created python program

---
+++ Cosc450_Project2_jpbutler0/src/cosc450_project2_jpbutler0.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="walter"
+__date__ ="$Nov 3, 2014 7:54:21 PM$"
+
+if __name__ == "__main__":
+    print "Hello World"
